29e5d182452e0fb813a2ec1685f4a7ed3a13eb8f
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-01-19 13:29:51 +0530

    page Setup Done. App Icon Done.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ImageBackground, Image } from 'react-native'
+import { Text, ImageBackground, Image } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
 import { images } from '@/constants/images'
@@ -9,8 +9,8 @@ const TabIcon = ({ focused, icon, title }: any ) => {
         return (
             <ImageBackground 
                 source={images.highlight} 
-                className='flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'>
-                    <Image source={icon} tintColour = "#151312" className="size-5" />
+                className='flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden'>
+                    <Image source={icon} tintColor = "#151312" className="size-5" />
                     <Text
                         className=' 
                         // text-secondary 
@@ -23,7 +23,28 @@ const TabIcon = ({ focused, icon, title }: any ) => {
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+    screenOptions={{
+        tabBarShowLabel: false,
+        tabBarIconStyle: {
+            width: '100%',
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center'
+        },
+        tabBarStyle: {
+            backgroundColor: '#0f0D23',
+            borderRadius: 50,
+            marginHorizontal: 20,
+            marginBottom: 36,
+            height: 52,
+            position: 'absolute',
+            overflow: 'hidden',
+            borderWidth: 1,
+            borderColor: '0f0d23'
+        }
+    }}
+    >
         <Tabs.Screen
         name="index"
         options={{
